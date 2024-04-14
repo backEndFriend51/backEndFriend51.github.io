767b6d19ad522401da224535536fa776905bab23
backEndFriend51 <rr.stoehr@gmx.de>
2024-04-14 20:53:24 +0200

    JavaScript animation

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ buttonElement.addEventListener('click', function (e) {
     console.log(e)
     alert("You clicked")
 })
+
+function animateText(textArea) {
+    let text = textArea.value;
+    let to = text.length,
+      from = 0;
+
+    animate({
+      duration: 5000,
+      timing: bounce,
+      draw: function(progress) {
+        let result = (to - from) * progress + from;
+        textArea.value = text.slice(0, Math.ceil(result))
+      }
+    });
+  }
+
+
+  function bounce(timeFraction) {
+    for (let a = 0, b = 1; 1; a += b, b /= 2) {
+      if (timeFraction >= (7 - 4 * a) / 11) {
+        return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+      }
+    }
+  }
