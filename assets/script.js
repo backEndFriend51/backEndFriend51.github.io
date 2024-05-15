9e317b28ff630ade6ae4faff34fc045420da287b
Tommy <spielhoelle@posteo.net>
2024-05-15 19:46:30 +0900

    chore: remove logs

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,3 @@
-console.log("hello world")
-const name = "Rainer"
-console.log(name)
-const age = 19
-if (age > 20) {
-  console.log("welcome")
-} else {
-  console.log("Too young")
-}
 const buttonElement = document.getElementById("clickhandler")
 buttonElement.addEventListener('click', function (e) {
   console.log(e)
@@ -46,7 +37,6 @@ function myTimer() {
   
   const elements = [...document.querySelectorAll(".imageholder img")];
   index = (index + 1) % elements.length;
-  console.log(index);
   elements.forEach(ele => {
 
     ele.classList.add("hidden");
